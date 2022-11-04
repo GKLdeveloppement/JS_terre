3916b918e0759c651ec01fe0404f79fa35e52a7d
Semih Gokol <gokol.semih@gmail.com>
2022-11-04 21:44:14 +0100

    fin de la 2ème épreuve

--- terre01.js
+++ terre01.js
@@ -3,7 +3,13 @@ Créez un programme qui affiche son nom de fichier.
 */
 
 function fileName(){
-
+    //Etape 1 : avoir le chemin du fichier
+    const urlFile = __filename
+    //Etape 2 : Découper la string par le /
+    let filenameArr = urlFile.split("/")
+    //Etape 3 : récupérer le dernier élément
+    let lastItem = filenameArr.pop()
+    console.log(lastItem)
 }
 
 fileName();
