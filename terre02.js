79cb8d6351baff508961ff72aadd31200f0f0d37
Semih Gokol <gokol.semih@gmail.com>
2022-11-04 21:55:30 +0100

    fin du 3eme exo

---
+++ terre02.js
@@ -0,0 +1,15 @@
+/*
+Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+*/
+
+function showArgs(){
+    //Etape 1: On récupère les arguments du terminal
+    let args = process.argv
+    let argsReturn = ""
+
+    //Etape 2 : On boucle en commençant par l'index 2 car pas besoin des 2 premiers arguments (node + filePath)
+    for (let i = 2; i < args.length; i++) {
+        console.log(args[i]);
+    }
+}
+showArgs();
