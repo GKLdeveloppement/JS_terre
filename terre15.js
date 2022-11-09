b6dc8052de360cc9c78ed4ca31ea58c4187861de
Semih Gokol <gokol.semih@gmail.com>
2022-11-10 00:29:18 +0100

    epreuve finis

---
+++ terre15.js
@@ -0,0 +1,14 @@
+/*
+Créez un programme qui célèbre votre victoire.
+
+Exemples d’utilisation :
+$> ruby exo.rb
+J’ai terminé l’Épreuve de la Terre et c’était [].
+$>
+*/
+
+function victory() {
+    console.log("J’ai terminé l’Épreuve de la Terre et c’était cool.");
+    return;
+}
+victory()
